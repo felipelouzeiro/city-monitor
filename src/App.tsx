b4e06704c72e74d871520c13bb5b8a31d7e54c76
felipe <fl.louzeiro@gmail.com>
2022-06-15 10:57:14 -0300

    :sparkles: feat: adicionado e configurado o react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './routes'
+
 export const App = () => {
-  return <div>teste</div>
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
 }
