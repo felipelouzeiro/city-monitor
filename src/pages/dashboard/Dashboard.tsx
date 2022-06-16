ebcf8acc694f9964d956894256b710f6cbcc169d
felipe <fl.louzeiro@gmail.com>
2022-06-16 15:28:01 -0300

    :sparkles: feat: ajustando responsividade do layout base das páginas

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,6 +2,11 @@ import { LayoutBaseDePagina } from '../../shared/layouts';
 
 export const Dashboard = () => {
   return (
-    <LayoutBaseDePagina titulo="Pagina inicial">Testando</LayoutBaseDePagina>
+    <LayoutBaseDePagina
+      titulo="Pagina inicial"
+      barraDeFerramentas={<>Barra de Ferramentas</>}
+    >
+      Testando
+    </LayoutBaseDePagina>
   );
 };
--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -11,22 +11,26 @@ import { useDrawerContext } from '../contexts';
 interface ILayoutBaseDePaginaProps {
   children: React.ReactNode;
   titulo: string;
+  barraDeFerramentas: React.ReactNode;
 }
 
 export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({
   children,
   titulo,
+  barraDeFerramentas,
 }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'));
   const { toggleDrawerOpen } = useDrawerContext();
+
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
       <Box
         padding={1}
         display="flex"
         alignItems="center"
-        height={theme.spacing(12)}
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}
         gap={1}
       >
         {smDown && (
@@ -35,10 +39,19 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({
           </IconButton>
         )}
 
-        <Typography variant="h5">{titulo}</Typography>
+        <Typography
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis"
+        >
+          {titulo}
+        </Typography>
       </Box>
-      <Box>Barra de ferramentas</Box>
-      <Box>{children}</Box>
+      {barraDeFerramentas && <Box>{barraDeFerramentas}</Box>}
+      <Box flex={1} overflow="auto">
+        {children}
+      </Box>
     </Box>
   );
 };
