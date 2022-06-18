dd33024500527823b7ce85052e9289a0b0fb767b
felipe <fl.louzeiro@gmail.com>
2022-06-18 16:05:33 -0300

    :sparkles: feat: implementando delay para impedir consultas desnecessárias

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -4,21 +4,25 @@ import { useSearchParams } from 'react-router-dom';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { FerramentasDaListagem } from '../../shared/components';
 import { PessoaService } from '../../shared/services/api/pessoas/PessoasService';
+import { useDebounce } from '../../shared/hooks';
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(3000);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    PessoaService.getAll(1, busca).then((result) => {
-      if (result instanceof Error) {
-        alert(result.message);
-      } else {
-        console.log(result);
-      }
+    debounce(() => {
+      PessoaService.getAll(1, busca).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          console.log(result);
+        }
+      });
     });
   }, [busca]);
 
