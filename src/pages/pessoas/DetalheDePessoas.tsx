90cf0d750225a15dc3d4fc0544b46ceb9eb173ec
felipe <fl.louzeiro@gmail.com>
2022-06-25 10:07:21 -0300

    :bug: ix: limpando formulário de nova pesoa cadastrada

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -40,6 +40,12 @@ export const DetalheDePessoas = () => {
           formRef.current?.setData(result);
         }
       });
+    } else {
+      formRef.current?.setData({
+        nomeCompleto: '',
+        email: '',
+        cidadeId: '',
+      });
     }
   }, [id]);
 
