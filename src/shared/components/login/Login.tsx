836a24f23dc29e1dc204fe384775dccc84f6cd19
felipe <fl.louzeiro@gmail.com>
2022-07-01 19:29:59 -0300

    :sparkles: feat: implementando restrição de acesso a aplicação

---
+++ src/shared/components/login/Login.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import { useAuthContext } from '../../contexts/AuthContext';
+
+interface ILoginProps {
+  children: React.ReactNode;
+}
+
+export const Login: React.FC<ILoginProps> = ({ children }) => {
+  const { isAuthenticated } = useAuthContext();
+  if (isAuthenticated) return <>{children}</>;
+
+  return <Box>Login</Box>;
+};
