f66de2487d63312921f668510b475c3a5a8b1d1e
felipe <fl.louzeiro@gmail.com>
2022-07-01 22:35:27 -0300

    :hammer: refactor: alterando url base

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -17,5 +17,5 @@ export const Environment = {
   /**
    * Url base de consulta dos dados dessa aplicação.
    */
-  URL_BASE: 'http://localhost:3333',
+  URL_BASE: 'https://json-server-api-mock.herokuapp.com/api/',
 };
